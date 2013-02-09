7e68020c58826a39e8859933086e55bf3d49da8a
Ryan Kulla <rkulla@gmail.com>
2013-02-09 13:52:13 -0800

    Initial commit

---
+++ vimogen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# vimogen.sh by Ryan Kulla <rkulla@gmail.com>
+# An opinionated shell script that believes you should install
+# your Vim plugins via Pathogen bundles/git repos and automates
+# installing and updated your bundles.
