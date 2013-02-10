3e0393cf739ca1c548a5e485a316b43edfbf8361
Ryan Kulla <rkulla@gmail.com>
2013-02-10 00:54:28 -0800

    Added install feature

---
+++ README.md
@@ -0,0 +1,26 @@
+
+Vimogen is an opinionated shell script that believes you should install
+your Vim plugins via Pathogen bundles/git repos and automates installing
+and updating your bundles.
+
+Installation
+============
+Create a manifest file called $HOME/.vimogen_repos that conists
+of just git repositories. (See the supplied example)
+
+Then mv vimogen.sh ~/bin/vimogen (or somewhere in your $PATH)
+
+Usage
+=====
+Run vimogen without arguments:
+
+    $ vimogen
+
+It will give you a menu of items to choose from. Such as 'Install' or 'Update'.
+
+If you choose Install, then it will a 'git clone' on all the git repositories 
+that you specified in ~/.vimogen_repos into your Pathogen dir (~/.vim/bundle).
+It will skip any directories that already exist, so you can append new plugins
+to the vimogen_repos manifest later and install them too.
+
+If you choose Update, then it will run a 'git pull' on the bundles.
--- vimogen.sh
+++ vimogen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # vimogen.sh by Ryan Kulla <rkulla@gmail.com>
-# An opinionated shell script that believes you should install
-# your Vim plugins via Pathogen bundles/git repos and automates
-# installing and updated your bundles.
+
+install_dir="$HOME/.vim/bundle"
+manifest_file="$HOME/.vimogen_repos"
 
 usage() {
     printf "Usage:\n"
@@ -10,10 +10,42 @@ usage() {
     exit
 }
 
-# TODO:
-# install command should allow installing newly added repos without redoing existing ones
+validate_environment() {
+    if [[ ! -d "$install_dir" ]]; then
+        printf "$install_dir doesn't exist. Please create it first.\n"
+        exit 0
+    fi
+
+    if [[ ! -f "$manifest_file" ]]; then
+        printf "$manifest_file doesn't exist. Please create it first.\n"
+        exit 0
+    fi
+}
+
 install() {
     printf "Installing...\n"
+    pushd . > /dev/null
+    local install_count=0
+
+    cd "$install_dir"
+
+    while read -r line; do
+        local basename=${line##*/}
+        local clone_dir="${basename%.*}"
+        if [[ ! -d "$install_dir/$clone_dir" ]]; then
+            git clone "$line"
+            install_count=$(( install_count+1 ))
+        fi
+    done < "$manifest_file"
+
+    popd > /dev/null
+
+    if [[ $install_count -eq 0 ]]; then
+        printf "Nothing new to install\n"
+    else
+        printf "Installed $install_count plugins\n"
+    fi
+    
     exit 0
 }
 
@@ -22,25 +54,30 @@ update() {
     exit 0
 }
 
+get_menu_opt() {
+    PS3="Select a menu option to perform: "
+    select option in Install Update Exit
+    do
+        case "$option" in
+            Install) 
+                install
+                ;;
+            Update) 
+                update
+                ;;
+            Exit) 
+                exit 0
+                ;;
+            *) 
+                echo  "Invalid selection"
+                ;;
+        esac
+    done
+}
+
 if (( $# > 0 )); then
     usage
+else
+    validate_environment
+    get_menu_opt
 fi
-
-PS3="Select a menu option to perform: "
-select option in Install Update Exit
-do
-    case "$option" in
-        Install) 
-            install
-            ;;
-        Update) 
-            update
-            ;;
-        Exit) 
-            exit 0
-            ;;
-        *) 
-            echo  "Invalid selection"
-            ;;
-    esac
-done
